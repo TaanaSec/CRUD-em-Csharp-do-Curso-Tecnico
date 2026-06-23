e557a816125321a9a64af2080d9b36e6083e6292
TaanaReserva <cys.mariana@gmail.com>
2026-06-23 16:26:15 -0300

    Parte visual em andamento

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,218 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnConsultarTodos = new System.Windows.Forms.Button();
+            this.btnExibir = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.txtCod = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtTele = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.BackColor = System.Drawing.Color.Black;
+            this.btnNovo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNovo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnNovo.Font = new System.Drawing.Font("Segoe UI Variable Small Semibol", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnNovo.ForeColor = System.Drawing.Color.White;
+            this.btnNovo.Location = new System.Drawing.Point(12, 139);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(130, 30);
+            this.btnNovo.TabIndex = 0;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = false;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.BackColor = System.Drawing.Color.Black;
+            this.btnConsultar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConsultar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnConsultar.Font = new System.Drawing.Font("Segoe UI Variable Small Semibol", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnConsultar.ForeColor = System.Drawing.Color.White;
+            this.btnConsultar.Location = new System.Drawing.Point(12, 175);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(130, 30);
+            this.btnConsultar.TabIndex = 1;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = false;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.Black;
+            this.btnEditar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEditar.Font = new System.Drawing.Font("Segoe UI Variable Small Semibol", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.ForeColor = System.Drawing.Color.White;
+            this.btnEditar.Location = new System.Drawing.Point(148, 139);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(130, 30);
+            this.btnEditar.TabIndex = 2;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            // 
+            // btnConsultarTodos
+            // 
+            this.btnConsultarTodos.BackColor = System.Drawing.Color.Black;
+            this.btnConsultarTodos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConsultarTodos.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnConsultarTodos.Font = new System.Drawing.Font("Segoe UI Variable Small Semibol", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnConsultarTodos.ForeColor = System.Drawing.Color.White;
+            this.btnConsultarTodos.Location = new System.Drawing.Point(148, 175);
+            this.btnConsultarTodos.Name = "btnConsultarTodos";
+            this.btnConsultarTodos.Size = new System.Drawing.Size(130, 30);
+            this.btnConsultarTodos.TabIndex = 3;
+            this.btnConsultarTodos.Text = "Consultar Todos";
+            this.btnConsultarTodos.UseVisualStyleBackColor = false;
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.BackColor = System.Drawing.Color.Black;
+            this.btnExibir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExibir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExibir.Font = new System.Drawing.Font("Segoe UI Variable Small Semibol", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnExibir.ForeColor = System.Drawing.Color.White;
+            this.btnExibir.Location = new System.Drawing.Point(284, 139);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(130, 30);
+            this.btnExibir.TabIndex = 4;
+            this.btnExibir.Text = "Exibir";
+            this.btnExibir.UseVisualStyleBackColor = false;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Black;
+            this.btnSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSair.Font = new System.Drawing.Font("Segoe UI Variable Small Semibol", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnSair.ForeColor = System.Drawing.Color.White;
+            this.btnSair.Location = new System.Drawing.Point(284, 175);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(130, 30);
+            this.btnSair.TabIndex = 5;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            // 
+            // txtCod
+            // 
+            this.txtCod.AccessibleDescription = "";
+            this.txtCod.AccessibleName = "";
+            this.txtCod.BackColor = System.Drawing.Color.Gray;
+            this.txtCod.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCod.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCod.ForeColor = System.Drawing.Color.White;
+            this.txtCod.Location = new System.Drawing.Point(12, 33);
+            this.txtCod.Name = "txtCod";
+            this.txtCod.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtCod.Size = new System.Drawing.Size(110, 23);
+            this.txtCod.TabIndex = 6;
+            // 
+            // txtNome
+            // 
+            this.txtNome.BackColor = System.Drawing.Color.Gray;
+            this.txtNome.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNome.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold);
+            this.txtNome.ForeColor = System.Drawing.Color.White;
+            this.txtNome.Location = new System.Drawing.Point(128, 33);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(286, 23);
+            this.txtNome.TabIndex = 7;
+            // 
+            // txtTele
+            // 
+            this.txtTele.BackColor = System.Drawing.Color.Gray;
+            this.txtTele.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTele.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold);
+            this.txtTele.ForeColor = System.Drawing.Color.White;
+            this.txtTele.Location = new System.Drawing.Point(12, 91);
+            this.txtTele.Name = "txtTele";
+            this.txtTele.Size = new System.Drawing.Size(402, 23);
+            this.txtTele.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Variable Display", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "CÓDIGO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Variable Display", 9F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(125, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "NOME DO CLIENTE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Variable Display", 9F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(9, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "TELEFONE DO CLIENTE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(433, 344);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTele);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtCod);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.btnConsultarTodos);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnNovo);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnConsultarTodos;
+        private System.Windows.Forms.Button btnExibir;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.TextBox txtCod;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtTele;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
